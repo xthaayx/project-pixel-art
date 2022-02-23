1f236ade9434385442fa9db3aad3d7d9dc2ca396
Thaysa <thay.fernnanda@gmail.com>
2022-02-23 19:25:07 -0300

    adiciona cor no pixel e botao limpar

--- script.js
+++ script.js
@@ -17,15 +17,16 @@ function quadro() {
     }
 }
 quadro();
-//cria variavel que puxa o id da paleta
+
+//crie variavel que puxa o id da paleta
 let selecionaColor = document.querySelector ('#color-palette');
 let cor = "black"
-//cria função pra mudar a cor
+
 function mudarCor(){
-    //cria um evento de click na paleta
+    //criei um evento de click na paleta
     selecionaColor.addEventListener('click', function(event){
         let corSelecionada = document.querySelector('.selected');
-        //encrementos de remover e adcionar evendo verifica no stackoverflow
+        //encremento de remove e add
         corSelecionada.classList.remove('selected');
         event.target.classList.add('selected');
         cor = event.target.id;
@@ -33,3 +34,28 @@ function mudarCor(){
 }
 mudarCor();
 
+// //função adc cor no pixel
+  function corNoPixel(){
+      let pixelArt = document.querySelectorAll('.pixel');
+      for(let indexCor =0; indexCor < pixelArt.length; indexCor +=1){
+          pixelArt[indexCor].addEventListener('click', function(e){
+              pixelArt[indexCor].style.backgroundColor = cor;
+          });
+      }
+  }
+  corNoPixel();
+
+//função de limpar
+  function clear() {
+     //variavel que puxa a classe pixel 
+     let pixel = document.getElementsByClassName('pixel');
+      for (let index = 0; index < pixel.length; index += 1) {
+        pixel[index].style.backgroundColor = "white";
+      }
+    }
+    //variavel que puxa o id do botão para adc o evento de click
+    let clearPixel = document.getElementById('clear-board');
+    clearPixel.addEventListener('click', clear); 
+
+
+    
