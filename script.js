7e56e9b8aa8f11e509499ef25714356b6bf6be94
Thaysa <thay.fernnanda@gmail.com>
2023-06-06 14:47:07 -0300

    atualização

--- script.js
+++ script.js
@@ -1,61 +1,57 @@
-console.log("teste");
+/* eslint-disable no-loop-func */
+console.log('teste');
 function quadro() {
-    //criando coluna
-    for (let index = 0; index < 5; index += 1) {
-        let divPai = document.querySelector('#pixel-board');
-        let criandoPixel = document.createElement('div');
-        divPai.appendChild(criandoPixel)[index];
-        // console.log("linha");
-        //criando linha
-        for (let index2 = 0; index2 < 5; index2 +=1 ) {
-            let divLinha = document.createElement('div');
-            divLinha.className = 'pixel';
+  // criando coluna
+  for (let index = 0; index < 5; index += 1) {
+    const divPai = document.querySelector('#pixel-board');
+    const criandoPixel = document.createElement('div');
+    divPai.appendChild(criandoPixel);
+    // criando linha
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      const divLinha = document.createElement('div');
+      divLinha.className = 'pixel';
 
-            divPai.appendChild(divLinha)[index];
-            // console.log("coluna");
-        }
+      divPai.appendChild(divLinha);
     }
+  }
 }
 quadro();
 
-//crie variavel que puxa o id da paleta
-let selecionaColor = document.querySelector ('#color-palette');
-let cor = "black"
+// crie variavel que puxa o id da paleta
+const selecionaColor = document.querySelector('#color-palette');
+let cor = 'black';
 
-function mudarCor(){
-    //criei um evento de click na paleta
-    selecionaColor.addEventListener('click', function(event){
-        let corSelecionada = document.querySelector('.selected');
-        //encremento de remove e add
-        corSelecionada.classList.remove('selected');
-        event.target.classList.add('selected');
-        cor = event.target.id;
-    })
+function mudarCor() {
+  // criei um evento de click na paleta
+  selecionaColor.addEventListener('click', (event) => {
+    const corSelecionada = document.querySelector('.selected');
+    // encremento de remove e add
+    corSelecionada.classList.remove('selected');
+    event.target.classList.add('selected');
+    cor = event.target.id;
+  });
 }
 mudarCor();
 
 // //função adc cor no pixel
-  function corNoPixel(){
-      let pixelArt = document.querySelectorAll('.pixel');
-      for(let indexCor =0; indexCor < pixelArt.length; indexCor +=1){
-          pixelArt[indexCor].addEventListener('click', function(e){
-              pixelArt[indexCor].style.backgroundColor = cor;
-          });
-      }
+function corNoPixel() {
+  const pixelArt = document.querySelectorAll('.pixel');
+  for (let indexCor = 0; indexCor < pixelArt.length; indexCor += 1) {
+    pixelArt[indexCor].addEventListener('click', () => {
+      pixelArt[indexCor].style.backgroundColor = cor;
+    });
   }
-  corNoPixel();
+}
+corNoPixel();
 
-//função de limpar
-  function clear() {
-     //variavel que puxa a classe pixel 
-     let pixel = document.getElementsByClassName('pixel');
-      for (let index = 0; index < pixel.length; index += 1) {
-        pixel[index].style.backgroundColor = "white";
-      }
-    }
-    //variavel que puxa o id do botão para adc o evento de click
-    let clearPixel = document.getElementById('clear-board');
-    clearPixel.addEventListener('click', clear); 
-
-
-    
+// função de limpar
+function clear() {
+  // variavel que puxa a classe pixel
+  const pixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
+}
+// variavel que puxa o id do botão para adc o evento de click
+const clearPixel = document.getElementById('clear-board');
+clearPixel.addEventListener('click', clear);
